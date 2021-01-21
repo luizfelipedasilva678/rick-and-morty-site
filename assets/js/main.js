486f610e225a34d8226347368ea798f6eaf921d7
Luiz <luizfelipedasilva678@gmail.com>
2021-01-20 23:30:03 -0300

    Pagition

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,36 @@
 (function () {
-    async function loadData(num) {
-        let data = await axios('https://rickandmortyapi.com/api/character/?page=num');
+    let count = 1;
+    let dataInfo;
 
-        createCard(data.data.results);
+    
+    document.addEventListener('click', (e) => {
+        e.preventDefault();
+        
+        if(e.target.className === 'next') {
+            loadData(dataInfo.next);
+        }
+
+        if(e.target.className === 'prev') {
+            loadData(dataInfo.prev);
+        }
+    });
+    
+    async function loadData(url = 'https://rickandmortyapi.com/api/character/?page=1') {
+        let data = await axios(url);
+        dataInfo = data.data.info;
+        createCard(data.data);
     }
 
+
     function createCard(data) {
-        let img;
-        let div;
-        let gallery = document.querySelector('.gallery');
+        let $imgs = document.querySelectorAll('.gallery div img');
+        let array = Array.from($imgs);
 
-        for (let values of data) {
-            div = document.createElement('div');
-            
-            img = document.createElement('img');
-            img.src = values.image;
-
-            div.appendChild(img);
-            gallery.appendChild(div);
-        }   
+        for(let i = 0; i < data.results.length; i++) {
+            array[i].src = data.results[i].image;
+        }
     }
 
 
     loadData();
-
 })();   
